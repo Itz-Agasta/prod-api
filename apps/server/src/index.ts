0c8b3c51399c78834873744069f902cfd77be724
Itz-Agasta <rupamgolui69@gmail.com>
2025-08-02 13:40:41 +0530

    refactor: reorganize middleware setup and remove unused route

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,27 +1,37 @@
-import "dotenv/config";
-import cors from "cors";
-import express from "express";
+import 'dotenv/config';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express, { urlencoded } from 'express';
+import helmet from 'helmet';
+import v1Routes from '@/routers/v1';
+import limiter from './lib/rate-limit';
 
 const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "",
-    methods: ["GET", "POST", "OPTIONS"],
+    origin: process.env.CORS_ORIGIN || '',
+    methods: ['GET', 'POST', 'OPTIONS'],
   })
 );
 
+// Middlewares
+app.use(urlencoded({ extended: true }));
+app.use(express.json());
+app.use(
+  compression({
+    threshold: 1024, // only compress res >= 1kb
+  })
+);
+app.use(cookieParser());
+app.use(helmet());
+app.use(limiter);
 
-
-
-app.use(express.json())
-
-
-app.get("/", (_req, res) => {
-  res.status(200).send("OK");
-});
+app.use('/api/v1', v1Routes);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
+  // biome-ignore lint/suspicious/noConsole: false positive, logging server start
   console.log(`Server is running on port ${port}`);
 });
