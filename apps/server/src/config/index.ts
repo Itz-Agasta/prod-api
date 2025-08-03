13985b443f05d14c19bb36bbcc61cd3a57c02e09
Itz-Agasta <rupamgolui69@gmail.com>
2025-08-03 20:01:33 +0530

    feat: add configuration file for environment variables

---
+++ apps/server/src/config/index.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+
+const config = {
+  PORT: process.env.PORT || 3000,
+  CORS_ORIGIN: process.env.CORS_ORIGIN || '',
+  DATABASE_URL: process.env.DATABASE_URL,
+};
+
+export default config;
