4e5dd55b4373065d447d785894f69cca03a769b5
Itz-Agasta <rupamgolui69@gmail.com>
2025-08-03 20:01:38 +0530

    feat: refactor server setup and enhance database connection handling

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -5,13 +5,15 @@ import cors from 'cors';
 import express, { urlencoded } from 'express';
 import helmet from 'helmet';
 import v1Routes from '@/routers/v1';
+import config from './config';
+import { connectToDb, disconnectFromDb } from './lib/mongoose';
 import limiter from './lib/rate-limit';
 
 const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || '',
+    origin: config.CORS_ORIGIN,
     methods: ['GET', 'POST', 'OPTIONS'],
   })
 );
@@ -28,10 +30,40 @@ app.use(cookieParser());
 app.use(helmet());
 app.use(limiter);
 
-app.use('/api/v1', v1Routes);
+// Server setup
+(async () => {
+  try {
+    await connectToDb();
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  // biome-ignore lint/suspicious/noConsole: false positive, logging server start
-  console.log(`Server is running on port ${port}`);
-});
+    app.use('/api/v1', v1Routes);
+    const port = config.PORT;
+    app.listen(port, () => {
+      // biome-ignore lint/suspicious/noConsole: false positive, logging server start
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (err) {
+    // biome-ignore lint/suspicious/noConsole: false positive, logging server start
+    console.error('Error during server setup:', err);
+  }
+
+  if (process.env.NODE_ENV === 'production') {
+    process.exit(1);
+  }
+})();
+
+// Handel server shutdown
+const handleServerShutdown = async () => {
+  try {
+    await disconnectFromDb();
+    // biome-ignore lint/suspicious/noConsole: false positive, logging server start
+    console.log('Server SHUTDOWN');
+    process.exit(0);
+  } catch (err) {
+    // biome-ignore lint/suspicious/noConsole: false positive, logging server start
+    console.log('Error during server shutdown', err);
+  }
+};
+
+// Listen for terminal signals ('SIGTERM` & `SIGINT`)
+process.on('SIGTERM', handleServerShutdown);
+process.on('SIGINT', handleServerShutdown);
